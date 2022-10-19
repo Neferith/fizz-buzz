f7e900803a72b3e8475b9874496e743c3cb5ee61
vivien <somite100>
2022-10-19 23:06:15 +0200

    Test unitaire Fix method utils

--- app/src/main/java/com/fizzbuzz/utils/FizzBuzzUtils.kt
+++ app/src/main/java/com/fizzbuzz/utils/FizzBuzzUtils.kt
@@ -14,22 +14,24 @@ object FizzBuzzUtils {
     /**
      * Generate fizzbuzz string of an index
      * @param index the index position in the list.
-     * @property item the fitbuzz parameters.
+     * @property item the fizzbuzz parameters.
      * @return The fizzbuzz string
      */
     fun generateFitBuzzString(index: Int, item: FizzbuzzMeta): String {
         // All multiples of int1 and int2 are replaced by str1str2
-        if (index % (item.int1 * item.int2) == 0) {
-            return item.str1 + item.str2
-        }
+       // if (index % (item.int1 * item.int2) == 0) {
+         //   return item.str1 + item.str2
+        //}
+
+        var str = ""
         // All multiples of int1 are replaced by str1
         if (index % item.int1 == 0) {
-            return item.str1
+            str += item.str1
         }
         // All multiples of int2 are replaced by str2
         if (index % item.int2 == 0) {
-            return item.str2
+            str += item.str2
         }
-        return index.toString()
+        return if(str.isEmpty()) index.toString() else str
     }
 }
--- app/src/test/java/com/fizzbuzz/FizzBuzzUtilsUnitTest.kt
+++ app/src/test/java/com/fizzbuzz/FizzBuzzUtilsUnitTest.kt
@@ -1,14 +1,14 @@
 package com.fizzbuzz
 
+import com.fizzbuzz.model.FizzbuzzMeta
 import com.fizzbuzz.utils.FizzBuzzUtils
 import org.junit.Test
 
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Test class FizzBuzzUtils.
  *
- * See [testing documentation](http://d.android.com/tools/testing).
  */
 class FizzBuzzUtilsUnitTest {
     @Test
@@ -18,5 +18,40 @@ class FizzBuzzUtilsUnitTest {
         assertEquals("2", FizzBuzzUtils.generateFitBuzzString(2,FizzBuzzUtils.defaultFitBuzz()))
         assertEquals("fizz", FizzBuzzUtils.generateFitBuzzString(3,FizzBuzzUtils.defaultFitBuzz()))
         assertNotEquals("3", FizzBuzzUtils.generateFitBuzzString(3,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("4", FizzBuzzUtils.generateFitBuzzString(4,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("buzz", FizzBuzzUtils.generateFitBuzzString(10,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("fizz", FizzBuzzUtils.generateFitBuzzString(6,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("fizzbuzz", FizzBuzzUtils.generateFitBuzzString(15,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("fizz", FizzBuzzUtils.generateFitBuzzString(27,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("fizzbuzz", FizzBuzzUtils.generateFitBuzzString(30,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("buzz", FizzBuzzUtils.generateFitBuzzString(35,FizzBuzzUtils.defaultFitBuzz()))
+        assertEquals("fizzbuzz", FizzBuzzUtils.generateFitBuzzString(45,FizzBuzzUtils.defaultFitBuzz()))
+
+       val darkVardor = FizzbuzzMeta(2,2,300,"dark"," vador")
+
+        assertEquals("1", FizzBuzzUtils.generateFitBuzzString(1,darkVardor))
+        assertEquals("dark vador", FizzBuzzUtils.generateFitBuzzString(2,darkVardor))
+        assertEquals("3", FizzBuzzUtils.generateFitBuzzString(3,darkVardor))
+        assertEquals("dark vador", FizzBuzzUtils.generateFitBuzzString(4,darkVardor))
+
+
+        val testmeta = FizzbuzzMeta(2,3,300,"test","meta")
+
+        assertEquals("1",
+            FizzBuzzUtils.generateFitBuzzString(1,testmeta))
+        assertEquals("test",
+            FizzBuzzUtils.generateFitBuzzString(2,testmeta))
+        assertEquals("meta",
+            FizzBuzzUtils.generateFitBuzzString(3,testmeta))
+        assertEquals("test", FizzBuzzUtils.generateFitBuzzString(4,testmeta))
+        assertEquals("5", FizzBuzzUtils.generateFitBuzzString(5,testmeta))
+        assertEquals("testmeta", FizzBuzzUtils.generateFitBuzzString(6,testmeta))
+        assertEquals("7", FizzBuzzUtils.generateFitBuzzString(7,testmeta))
+        assertEquals("test", FizzBuzzUtils.generateFitBuzzString(8,testmeta))
+        assertEquals("meta", FizzBuzzUtils.generateFitBuzzString(9,testmeta))
+        assertEquals("test", FizzBuzzUtils.generateFitBuzzString(10,testmeta))
+        assertEquals("11", FizzBuzzUtils.generateFitBuzzString(11,testmeta))
+        assertEquals("testmeta", FizzBuzzUtils.generateFitBuzzString(12,testmeta))
+
     }
 }

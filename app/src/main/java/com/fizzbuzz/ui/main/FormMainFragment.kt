dd363db4066d95610643033227c01e6288bc0ab7
vivien <somite100>
2022-10-20 09:42:35 +0200

    Clean code

--- app/src/main/java/com/fizzbuzz/ui/main/FormMainFragment.kt
+++ app/src/main/java/com/fizzbuzz/ui/main/FormMainFragment.kt
@@ -31,8 +31,8 @@ class FormMainFragment : Fragment() {
 
     private fun goToResultActivity() {
         activity.let {
-            val intent = Intent (it, ResultActivity::class.java)
-            intent.putExtra("meta",viewModel.getCurrentItem())
+            val intent = Intent(it, ResultActivity::class.java)
+            intent.putExtra("meta", viewModel.getCurrentItem())
             it?.startActivity(intent)
         }
     }
@@ -41,97 +41,105 @@ class FormMainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         configureViewModel()
-
         configureForm(view)
 
     }
-
 
     private fun configureViewModel() {
         viewModel = ViewModelProvider(this)[FormMainViewModel::class.java]
     }
 
     private fun configureInput(
-        inView:View,
-        id:Int,
-        defaultValue:String,
+        inView: View,
+        id: Int,
+        defaultValue: String,
         onChange: (value: String) -> FormException?
     ) {
 
-
         val layout = inView.findViewById<TextInputLayout>(id)
-
-
         val int1View = layout.editText
         int1View?.setText(defaultValue)
 
         int1View?.doOnTextChanged { text, _, _, _ ->
-           val exception = onChange(text.toString())
-            submitError(layout,exception)
+            val exception = onChange(text.toString())
+            submitError(layout, exception)
         }
-
-
-
 
     }
 
     private fun submitError(layout: TextInputLayout, exception: FormException?) {
-        if(exception != null) {
+        if (exception != null) {
             layout.error = exception.message
             submitButton.visibility = View.GONE
         } else {
             layout.error = null
             submitButton.visibility = View.VISIBLE
         }
-
-
     }
 
-    private lateinit var submitButton:Button
-    private fun configureForm(inView:View) {
+    private lateinit var submitButton: Button
+    private fun configureForm(inView: View) {
 
-        configureInput(inView,R.id.textInputLayoutInt1,viewModel.getCurrentItem().int1.toString()) {
-           try {
-               viewModel.updateInt1(it)
-           }catch (e:FormException) {
-               return@configureInput e
-           }
+        configureInput(
+            inView,
+            R.id.textInputLayoutInt1,
+            viewModel.getCurrentItem().int1.toString()
+        ) {
+            try {
+                viewModel.updateInt1(it)
+            } catch (e: FormException) {
+                return@configureInput e
+            }
             return@configureInput null
         }
 
-        configureInput(inView,R.id.textInputLayoutInt2,viewModel.getCurrentItem().int2.toString()) {
+        configureInput(
+            inView,
+            R.id.textInputLayoutInt2,
+            viewModel.getCurrentItem().int2.toString()
+        ) {
             try {
                 viewModel.updateInt2(it)
-            }catch (e:FormException) {
+            } catch (e: FormException) {
                 return@configureInput e
             }
             return@configureInput null
         }
 
-        configureInput(inView,R.id.textInputLayoutLimit,viewModel.getCurrentItem().limit.toString()) {
+        configureInput(
+            inView,
+            R.id.textInputLayoutLimit,
+            viewModel.getCurrentItem().limit.toString()
+        ) {
             try {
                 viewModel.updateLimit(it)
-            }catch (e:FormException) {
+            } catch (e: FormException) {
                 return@configureInput e
             }
             return@configureInput null
         }
 
-        configureInput(inView,R.id.textInputLayoutStr1,viewModel.getCurrentItem().str1) {
+        configureInput(
+            inView,
+            R.id.textInputLayoutStr1,
+            viewModel.getCurrentItem().str1
+        ) {
 
-                viewModel.updateStr1(it)
+            viewModel.updateStr1(it)
 
             return@configureInput null
         }
 
-        configureInput(inView,R.id.textInputLayoutStr2,viewModel.getCurrentItem().str2) {
+        configureInput(
+            inView,
+            R.id.textInputLayoutStr2,
+            viewModel.getCurrentItem().str2
+        ) {
 
             viewModel.updateStr2(it)
 
             return@configureInput null
         }
-
-
 
         submitButton = inView.findViewById(R.id.submit)
         submitButton.setOnClickListener { goToResultActivity() }
